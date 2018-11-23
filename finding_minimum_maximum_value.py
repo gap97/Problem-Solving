95de101ce08038a8b5a47e042776bb575da894b7
gap97 <44805452+gap97@users.noreply.github.com>
2018-11-23 12:40:20 +0900

    Update finding_minimum_maximum_value.py

--- finding_minimum_maximum_value.py
+++ finding_minimum_maximum_value.py
@@ -1,21 +1,21 @@
 size = input
 
-student_score = []
+item = []
 
 i = 1
 
 minimun_index = 0
-minimun_value = student_score[0]
+minimun_value = item[0]
 maximun_index = 0
-maximun_index = student_score[0]
+maximun_index = item[0]
 
 while (i < size) :
-    if (student_score[i] < minumum_value):
-        minumum_value = student_score[i]
+    if (item[i] < minumum_value):
+        minumum_value = item[i]
         minumum_index=i
         
     else if (student_score[i] > maximum_value):
-        maximum_value = student_score[i]
+        maximum_value = item[i]
         maximum_index = i
         
     else i = i + 1
